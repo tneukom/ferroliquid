fddb03a120e3672d3bf9b4015566a39b6a3e8416
Tobias Neukom <tneukom@gmail.com>
2026-03-21 17:59:58 +0100

    Disable drawing solid bitmap in background

--- src/painting/solid_painter.rs
+++ src/painting/solid_painter.rs
@@ -84,7 +84,8 @@ impl SolidPainter {
     }
 
     pub unsafe fn paint<'a>(&self, gl: &glow::Context) {
-        self.blit_painter.draw(gl, &self.solid_texture, true);
+        // Paint actual solid bitmap in background
+        // self.blit_painter.draw(gl, &self.solid_texture, true);
 
         self.distance_field_painter
             .draw(gl, &self.signed_distance_texture);
